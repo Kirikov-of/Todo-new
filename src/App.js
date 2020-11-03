23a4c9951225796a2d9601d6987f4f127362fad9
kirikov-of <nekey143@yandex.ru>
2020-11-03 21:47:32 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Categories from "./components/Categories/Categories";
 import List from "./components/List";
-import { useHistory } from "react-router-dom";
+import { useHistory, Route, useLocation } from "react-router-dom";
 import axios from "axios";
 
 import "./app.scss";
 
 function App() {
   let history = useHistory();
+  let location = useLocation()
   
   const [category, setCategory] = React.useState(null);
   const [colors, setColors] = React.useState(null);
@@ -24,16 +25,6 @@ function App() {
     });
   }, []);
 
-  React.useEffect(() => {
-    const categoryId = history.location.pathname.split("category/")[1];
-    if (category) {
-      const list = category.find(
-        (cat) => cat.id === Number(categoryId)
-      );
-      setActiveItem(list);
-    }
-  }, [category, history.location.pathname]);
-
   const addCategory = (obj) => {
     const list = [...category, obj];
     setCategory(list);
@@ -49,19 +40,30 @@ function App() {
     setCategory(newlist);
   };
 
+  React.useEffect(() => {
+    const categoryId = history.location.pathname.split('categories/')[1]
+    if(category) {
+      const cat = category.find(category => category.id === Number(categoryId))
+      setActiveItem(cat);
+    }
+  }, [category, history.location.pathname])
+
   return (
     <div className="todo">
       <Categories
-        onClickItem={(item) => setActiveItem(item)}
+        onClickItem={category => {
+          history.push(`/categories/${category.id}`)
+        }}
         activeItem={activeItem}
         items={category}
         addCategory={addCategory}
         colors={colors}
       />
-
+      <Route path='/'>
       {category && activeItem && (
         <List list={activeItem} category={activeItem} onAddTask={onAddTask} />
       )}
+      </Route>
     </div>
   );
 }
